5bd643c791f3cba3fda8a5f91ebad25f9c5bfdcb
madwed <i.madwed@gmail.com>
2015-07-17 16:35:16 +0000

    reinit database, changed getting images so they are gotten directly from the s3 box

--- models/index.js
+++ models/index.js
@@ -3,49 +3,45 @@ var Promise = require("bluebird");
 mongoose.connect("mongodb://localhost/madpainter");
 mongoose.connection.on("error", console.error.bind(console, "connection error:"));
 
-var AWS = require("aws-sdk");
-var s3 = new AWS.S3();
-
+// var AWS = require("aws-sdk");
+// var s3 = new AWS.S3();
 
 
 var drawingSchema = new mongoose.Schema({
 	mainAuthor: String,
 	subAuthor: String,
-	dimensions: String,
-	link: String,
 	lastAccessed: {type: Date, default: Date.now }
 });
 
-drawingSchema.methods.setLink = function () {
-	this.link = this._id + "";
-};
-
-drawingSchema.pre("save", function (next) {
-	this.setLink();
-	next();
+drawingSchema.virtual("link").get(function () {
+	return "https://s3.amazonaws.com/madpainter/drawings/" + this._id;
 });
 
-var s3FindImage = function(imgData){
-	return new Promise(function(resolve, reject){
-		s3.getObject({
-			Bucket: "/madpainter/drawings", 
-			Key: imgData.link,
-			ResponseContentType: 'utf8'
-		}, function(err, image){
-			if(err) { reject(err); }
-			else {
-				resolve({data: imgData, image: image.Body.toString("utf8")});
-			}
-		});
-	});
-};
+//Promisified get object, actually retrieves 
+
+// var s3FindImage = function(imgData){
+// 	return new Promise(function(resolve, reject){
+// 		s3.getObject({
+// 			Bucket: "/madpainter/drawings", 
+// 			Key: imgData.link,
+// 			ResponseContentType: 'utf8'
+// 		}, function(err, image){
+// 			if(err) { reject(err); }
+// 			else {
+// 				resolve({data: imgData, image: image.Body.toString("utf8")});
+// 			}
+// 		});
+// 	});
+// };
 
 
 drawingSchema.statics.loadImages = function (limit) {
 	//return a thumbnail version of drawing
-	return this.find().limit(limit).exec().then(function (images) {
-		return Promise.map(images, s3FindImage);
-	});
+	return this.find().limit(limit).exec();
+
+	// .then(function (images) {
+	// 	return Promise.map(images, s3FindImage);
+	// });
 };
 
 var Drawing = mongoose.model("Drawing", drawingSchema);
